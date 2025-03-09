4652184a1cc6e7a8556b9030a8cc2aad2055c39b
Luan Ramos <luan.developer@gmail.com>
2025-03-09 20:55:17 -0300

    init auth implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TempModule } from './temp/temp.module';
 import { DbModule } from './db/db.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, TempModule, DbModule],
+  imports: [
+    UserModule, 
+    TempModule, 
+    DbModule, 
+    AuthModule
+  ],
   controllers: [],
   providers: [],
 })
